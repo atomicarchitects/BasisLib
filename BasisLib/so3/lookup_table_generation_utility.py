23043a8fdfc1c53de07846f892b2d7d51e0c30cf
Mit <53411468+mitkotak@users.noreply.github.com>
2024-05-05 22:36:12 -0400

    rewrote clebsch_gordan

--- BasisLib/so3/lookup_table_generation_utility.py
+++ BasisLib/so3/lookup_table_generation_utility.py
@@ -17,48 +17,48 @@ def _load_lookup_table_from_disk(
     load_from_cache: Callable[[IO[bytes]], Tuple[int, Dict[str, Any]]],
     init_empty_lookup_table: Callable[[int], Dict[str, Any]],
 ) -> Tuple[int, Dict[str, Any]]:
-    """Load a lookup table from disk.
+  """Load a lookup table from disk.
 
-    Args:
-      max_degree: Maximum degree for which values should exist in the returned
-        lookup table.
-      lookup_table_name: Name of the lookup table (used only for printing error
-        messages).
-      config_cache_path: Path to a cached file on disk, which can be specified
-        using the Config class.
-      package_cache_path: Path to the cached file on disk included with the e3x
-        package, which is used as a fallback if no config_cache_path is provided
-        (or does not exist yet).
-      load_from_cache: Function that reads the lookup table from a binary file
-        handle and returns a tuple consisting of the stored max_degree and the
-        loaded lookup table.
-      init_empty_lookup_table: Function that returns an empty version of the
-        lookup table (in case all attempts to load a cached version from disk
-        fail).
+  Args:
+    max_degree: Maximum degree for which values should exist in the returned
+      lookup table.
+    lookup_table_name: Name of the lookup table (used only for printing error
+      messages).
+    config_cache_path: Path to a cached file on disk, which can be specified
+      using the Config class.
+    package_cache_path: Path to the cached file on disk included with the e3x
+      package, which is used as a fallback if no config_cache_path is provided
+      (or does not exist yet).
+    load_from_cache: Function that reads the lookup table from a binary file
+      handle and returns a tuple consisting of the stored max_degree and the
+      loaded lookup table.
+    init_empty_lookup_table: Function that returns an empty version of the
+      lookup table (in case all attempts to load a cached version from disk
+      fail).
 
-    Returns:
-      A tuple consisting of an int with the stored max_degree in the lookup table
-      and the loaded lookup table itself.
-    """
-    # Load cached lookup table from path specified in Config.
-    if config_cache_path is not None and config_cache_path.exists():
-        with config_cache_path.open("rb") as f:
-            return load_from_cache(f)
-    else:  # Load pre-computed lookup table included with package.
-        try:
-            f = io.BytesIO(pkgutil.get_data(__name__, package_cache_path))
-            return load_from_cache(f)
-        except (zipfile.BadZipFile, OSError, IOError, KeyError, ValueError):
-            logging.exception(
-                (
-                    "Failed to load pre-computed %s lookup table included with the %s"
-                    " package (data may be corrupted). Consider re-installing the"
-                    " package to fix this problem."
-                ),
-                lookup_table_name,
-                __name__,
-            )
-            return -1, init_empty_lookup_table(max_degree)
+  Returns:
+    A tuple consisting of an int with the stored max_degree in the lookup table
+    and the loaded lookup table itself.
+  """
+  # Load cached lookup table from path specified in Config.
+  if config_cache_path is not None and config_cache_path.exists():
+    with config_cache_path.open('rb') as f:
+      return load_from_cache(f)
+  else:  # Load pre-computed lookup table included with package.
+    try:
+      f = io.BytesIO(pkgutil.get_data(__name__, package_cache_path))
+      return load_from_cache(f)
+    except (zipfile.BadZipFile, OSError, IOError, KeyError, ValueError):
+      logging.exception(
+          (
+              'Failed to load pre-computed %s lookup table included with the %s'
+              ' package (data may be corrupted). Consider re-installing the'
+              ' package to fix this problem.'
+          ),
+          lookup_table_name,
+          __name__,
+      )
+      return -1, init_empty_lookup_table(max_degree)
 
 
 def _print_cache_usage_information(
@@ -69,57 +69,57 @@ def _print_cache_usage_information(
     lookup_table_name: str,
     pregeneration_name: str,
 ) -> None:
-    """Print information about the possibility to use disk caches.
+  """Print information about the possibility to use disk caches.
 
-    Calculating lookup tables for high degrees is very computationally intensive.
-    The e3x library supports saving/loading lookup tables to disk, which is
-    generally preferable compared to calculating them on the fly for each run.
-    However, this feature must be explicitly enabled: This function prints an
-    explanation of how to enable the disk caching feature.
+  Calculating lookup tables for high degrees is very computationally intensive.
+  The e3x library supports saving/loading lookup tables to disk, which is
+  generally preferable compared to calculating them on the fly for each run.
+  However, this feature must be explicitly enabled: This function prints an
+  explanation of how to enable the disk caching feature.
 
-    Args:
-      lstart: Degree from which the generation of lookup table entries starts.
-      max_degree: Maximum degree for which entries in the lookup table will be
-        generated.
-      config_cache_path: Path to a cached file on disk, which can be specified
-        using the Config class.
-      set_cache_method_name: Name of the method which can be used to enable disk
-        caching for the given lookup table.
-      lookup_table_name: Name of the lookup table.
-      pregeneration_name: Name of the module that can be called for pregeneration.
-    """
-    if config_cache_path is None:
-        logging.warning(
-            (
-                "Generating %s lookup table with values for degrees up to"
-                " max_degree=%d (starting from degree=%d). Calculating the %s"
-                " lookup table is computationally expensive and will be repeated on"
-                " re-runs. It might be preferable to cache the results on disk and"
-                " load (instead of re-generate) them on successive runs. To enable"
-                " disk caching, call 'e3x.Config.%s(<path>)' at the start of your"
-                " program to specify a disk location for saving/loading the lookup"
-                " table. It is recommended to run 'python -m %s --path <path>"
-                " --num_processes <#cpu>' to pre-generate the lookup table with"
-                " multiple processes in parallel for faster results (preferably on"
-                " a machine with many CPUs)."
-            ),
-            lookup_table_name,
-            max_degree,
-            lstart,
-            lookup_table_name,
-            set_cache_method_name,
-            pregeneration_name,
-        )
-    else:
-        logging.info(
-            (
-                "Generating %s lookup table with values for degrees up to "
-                "max_degree=%d (starting from degree=%d)."
-            ),
-            lookup_table_name,
-            max_degree,
-            lstart,
-        )
+  Args:
+    lstart: Degree from which the generation of lookup table entries starts.
+    max_degree: Maximum degree for which entries in the lookup table will be
+      generated.
+    config_cache_path: Path to a cached file on disk, which can be specified
+      using the Config class.
+    set_cache_method_name: Name of the method which can be used to enable disk
+      caching for the given lookup table.
+    lookup_table_name: Name of the lookup table.
+    pregeneration_name: Name of the module that can be called for pregeneration.
+  """
+  if config_cache_path is None:
+    logging.warning(
+        (
+            'Generating %s lookup table with values for degrees up to'
+            ' max_degree=%d (starting from degree=%d). Calculating the %s'
+            ' lookup table is computationally expensive and will be repeated on'
+            ' re-runs. It might be preferable to cache the results on disk and'
+            ' load (instead of re-generate) them on successive runs. To enable'
+            " disk caching, call 'e3x.Config.%s(<path>)' at the start of your"
+            ' program to specify a disk location for saving/loading the lookup'
+            " table. It is recommended to run 'python -m %s --path <path>"
+            " --num_processes <#cpu>' to pre-generate the lookup table with"
+            ' multiple processes in parallel for faster results (preferably on'
+            ' a machine with many CPUs).'
+        ),
+        lookup_table_name,
+        max_degree,
+        lstart,
+        lookup_table_name,
+        set_cache_method_name,
+        pregeneration_name,
+    )
+  else:
+    logging.info(
+        (
+            'Generating %s lookup table with values for degrees up to '
+            'max_degree=%d (starting from degree=%d).'
+        ),
+        lookup_table_name,
+        max_degree,
+        lstart,
+    )
 
 
 def _save_lookup_table_to_disk(
@@ -127,23 +127,23 @@ def _save_lookup_table_to_disk(
     lookup_table_name: str,
     config_cache_path: epath.Path,
 ) -> None:
-    """Save a lookup table to disk.
+  """Save a lookup table to disk.
 
-    Args:
-      lookup_table: A dictionary containing the values that should be saved to
-        disk.
-      lookup_table_name: Name of the lookup table (used only for printing).
-      config_cache_path: Path to the cache file on disk, which can be specified
-        using the Config class.
-    """
-    logging.info("Saving %s lookup table to disk.", lookup_table_name)
-    if config_cache_path is not None:
-        try:
-            with config_cache_path.open("wb") as f:
-                np.savez_compressed(f, **lookup_table)
-        except (OSError, IOError):
-            logging.exception(
-                "Failed to save %s lookup table to '%s'. Continuing anyway...",
-                lookup_table_name,
-                config_cache_path,
-            )
+  Args:
+    lookup_table: A dictionary containing the values that should be saved to
+      disk.
+    lookup_table_name: Name of the lookup table (used only for printing).
+    config_cache_path: Path to the cache file on disk, which can be specified
+      using the Config class.
+  """
+  logging.info('Saving %s lookup table to disk.', lookup_table_name)
+  if config_cache_path is not None:
+    try:
+      with config_cache_path.open('wb') as f:
+        np.savez_compressed(f, **lookup_table)
+    except (OSError, IOError):
+      logging.exception(
+          "Failed to save %s lookup table to '%s'. Continuing anyway...",
+          lookup_table_name,
+          config_cache_path,
+      )
